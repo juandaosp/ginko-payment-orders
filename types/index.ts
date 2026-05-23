c77721cbb50653cef08ff7788632cce2ec52599c
MacBook <macbook@MacBooks-MacBook-Air.local>
2026-05-23 17:02:06 -0500

    feat(US-1.2): define PaymentOrder interface and OrderStatus type

---
+++ types/index.ts
@@ -0,0 +1,10 @@
+export type OrderStatus = "BORRADOR" | "APROBADA" | "RECHAZADA" | "PAGADA";
+
+export interface PaymentOrder {
+  id: string;
+  providerName: string;
+  amount: number; // En pesos colombianos
+  concept: string;
+  createdAt: string; // ISO Date string
+  status: OrderStatus;
+}
