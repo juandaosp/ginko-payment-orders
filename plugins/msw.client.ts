de6ba8ec7f5408dbb4943d5fd7b13ab2208c0fee
Juan David Ospina <juandaosp12@gmail>
2026-05-24 18:18:31 -0500

    MSW client will work in prod too

--- plugins/msw.client.ts
+++ plugins/msw.client.ts
@@ -1,11 +1,8 @@
 export default defineNuxtPlugin(async (nuxtApp) => {
-  if (process.env.NODE_ENV === "development") {
-    const { worker } = await import("../mocks/browser");
+  const { worker } = await import("../mocks/browser");
 
-    await worker.start({
-      onUnhandledRequest: "bypass",
-    });
-
-    console.log("🚀 MSW Mock Service Worker iniciado y listo para interceptar");
-  }
+  await worker.start({
+    onUnhandledRequest: "bypass",
+  });
+  console.log("🚀 MSW funcionando en producción (Modo Demo)");
 });
